f89c060ff71309a6eb03c27ace9669bbf90bd8fb
HATTORIULTRA <throwbvck@gmail.com>
2025-01-27 19:55:11 +0500

    test commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Test from "./components/Test.tsx";
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+import s from './Test.module.scss'
+
+function Test() {
+    return (
+        <div className={s.test}>
+            <h2>Test</h2>
+        </div>
+    )
+}
+
+export default Test
